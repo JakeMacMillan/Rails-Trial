4e93e98f19a63e4527d93af9452fe137e28760ae
JakeMacMillan <houdif@gmail.com>
2016-08-31 16:03:02 -0700

    progress update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,37 @@
 class GamesController < ApplicationController
 	def new
 	end
+
+	def create
+		@game = Game.new(params.require(:game).permit(:title, :description, :genre))
+
+		if @game.save
+			redirect_to @game
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@game = Game.find(params[:id])
+	end
+
+	def index
+		@games = Game.all
+	end
+
+	def update
+		@game = Game.find(params[:id])
+
+		if @game.update(game_params)
+			redirect_to @game
+		else
+			render 'edit'
+		end
+	end
+
+	private
+		def game_params
+			params.require(:game).permit(:title, :description, :genre)
+		end
 end
